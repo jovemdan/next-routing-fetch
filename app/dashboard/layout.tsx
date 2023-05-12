21a628a007559112b43fca9f837056f60f515772
jovemdan <danilogdev@gmail.com>
2023-05-11 21:38:11 -0300

    feat: next routing fetch exemples

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,32 @@
-export default function RootLayout({
+import Link from 'next/link'
+import { use } from 'react'
+
+async function getData() {
+  const res = await fetch('http://localhost:3001/products')
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const products = use(getData())
   return (
     <div>
-      <div className='p-4 border'>Layout Dash</div>
+      <ul>
+        {products?.map((product: any) => (
+          <li key={product.id}>
+            <Link href={`/dashboard/${product.id}`}>
+              {product.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
       <section>{children}</section>
     </div>
   )
